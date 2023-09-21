bddaef47f3dfbb28f3382059267087da567d2b71
Myat Su Mon <myatsumon2531999@gmail.com>
2023-09-21 16:58:48 +0630

    graphql setup

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,34 +1,46 @@
-import { NavLinks } from '@/constants'
-import Image from 'next/image'
-import Link from 'next/link'
-import AuthProviders from './AuthProviders'
-import { getCurrentUser } from '@/lib/session'
-import { Button } from './ui/button'
+import { NavLinks } from "@/constants";
+import { getCurrentUser } from "@/lib/session";
+import Image from "next/image";
+import Link from "next/link";
+import AuthProviders from "./AuthProviders";
 
 const Navbar = async () => {
   const session = await getCurrentUser();
-  return (
-    <nav className='flexBetween navbar'>
-        <div className="flex-1 flexStart gap-10">
-            <Link href="/">
-                <Image src="/logo.svg" width={115} height={43} alt='Flexibble' />
-            </Link>
-            <ul className="xl:flex hidden text-sm gap-7">
-                {NavLinks.map(link => (<Link href={link.href} key={link.key} >{link.text}</Link>))}
-            </ul>
-        </div>
-        <div className='flexCenter gap-4'>
-            {session ? (
-              <>
-              userphoto
-              <Link href="/create-projec">Share Work</Link>
-              </>
-            ): (
-              <AuthProviders/>
-            )}
-        </div>
-    </nav >
-  )
-}
 
-export default Navbar
+  return (
+    <nav className="flexBetween navbar">
+      <div className="flex-1 flexStart gap-10">
+        <Link href="/">
+          <Image src="/logo.svg" width={115} height={43} alt="Flexibble" />
+        </Link>
+        <ul className="xl:flex hidden text-sm gap-7">
+          {NavLinks.map((link) => (
+            <Link href={link.href} key={link.key}>
+              {link.text}
+            </Link>
+          ))}
+        </ul>
+      </div>
+      <div className="flexCenter gap-4">
+        {session?.user ? (
+          <>
+            {session?.user?.image && (
+              <Image
+                src={session?.user?.image}
+                width={40}
+                height={40}
+                alt={session?.user?.name}
+                className="rounded-full"
+              />
+            )}
+            <Link href="/create-projec">Share Work</Link>
+          </>
+        ) : (
+          <AuthProviders />
+        )}
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
--- lib/session.ts
+++ lib/session.ts
@@ -1,10 +1,10 @@
-import { getServerSession } from "next-auth";
-import { NextAuthOptions, User } from "next-auth";
+import { SessionInterface, UserProfile } from "@/common.type";
+import jsonwebtoken from 'jsonwebtoken';
+import { NextAuthOptions, User, getServerSession } from "next-auth";
 import { AdapterUser } from "next-auth/adapters";
-import GoogleProvider from 'next-auth/providers/google'
-import jsonwebtoken from 'jsonwebtoken'
 import { JWT } from "next-auth/jwt";
-import { SessionInterface } from "@/common.type";
+import GoogleProvider from 'next-auth/providers/google';
+import { createUser, getUser } from "./actions";
 
 export const authOptions: NextAuthOptions = {
     providers: [
@@ -15,10 +15,16 @@ export const authOptions: NextAuthOptions = {
     ],
     jwt: {
         encode: ({secret, token}) => {
-
+            const encodedToken = jsonwebtoken.sign({
+                ...token,
+                iss: 'grafbase',
+                exp: Math.floor(Date.now() / 1000 ) + 60 * 60
+            }, secret);
+            return encodedToken;
         },
         decode: async ({secret, token}) => {
-            
+            const decodedToken = jsonwebtoken.verify(token!, secret) as JWT;
+            return decodedToken;
         }
     }, 
     theme: {
@@ -27,13 +33,38 @@ export const authOptions: NextAuthOptions = {
     },
     callbacks: {
         async session({session}) {
+            const email = session?.user?.email as string;
+
+            try {
+                const data = await getUser(email) as {user?: UserProfile}
+
+                const newSession = {
+                    ...session,
+                    user: {
+                        ...session.user,
+                        ...data?.user
+                    }
+                }
+                return newSession;
+            } catch (error) {
+                console.log('Error retrieving user data', error)
+            }
+
             return session
         },
         async signIn({user}: {user: AdapterUser | User}) {
             try {
                 // get the user if they exist
+                const userExists = await getUser(user?.email as string) as {user?: UserProfile}
                 
                 // if they don't exist, create them
+                if (!userExists.user) {
+                    await createUser(
+                        user.name as string,
+                        user.email as string,
+                        user.image as string
+                    )
+                }
 
                 return true
             } catch (error) {
